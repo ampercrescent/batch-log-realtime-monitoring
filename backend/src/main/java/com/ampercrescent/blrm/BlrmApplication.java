dd7a298d749c0a1b216b3f4464845195ea252cde
sgkim1234 <sgkim2@seegene.com>
2025-02-12 15:30:57 +0900

    Initial commit

---
+++ backend/src/main/java/com/ampercrescent/blrm/BlrmApplication.java
@@ -0,0 +1,13 @@
+package com.ampercrescent.blrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlrmApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ampercrescent/blrm/BlrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ampercrescent.blrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
